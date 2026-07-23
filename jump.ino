7884106dd474c5113beb617052ad23b9557977a6
AngelaRodH <arodri10@socalgas.com>
2026-07-23 14:18:41 -0700

    Add files via upload

--- jump.ino
+++ jump.ino
@@ -1,9 +1,17 @@
-# Scoreboard
-pen = turtle.Turtle()
-pen.speed(0)
-pen.shape("square")
-pen.color("white")
-pen.penup()
-pen.hideturtle()
-pen.goto(0, 260)
-pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
+#include <LiquidCrystal.h>
+
+#define PIN_BUTTON 2
+#define PIN_AUTOPLAY 1
+#define PIN_READWRITE 10
+#define PIN_CONTRAST 12
+
+#define SPRITE_RUN1 1
+#define SPRITE_RUN2 2
+#define SPRITE_JUMP 3
+#define SPRITE_JUMP_UPPER '.'         // Use the '.' character for the head
+#define SPRITE_JUMP_LOWER 4
+#define SPRITE_TERRAIN_EMPTY ' '      // User the ' ' character
+#define SPRITE_TERRAIN_SOLID 5
+#define SPRITE_TERRAIN_SOLID_RIGHT 6
+  }
+}
